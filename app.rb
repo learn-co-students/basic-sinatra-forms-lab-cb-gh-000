1bfc0d447027a596e6077211af1798e6f7a64249
Adrian Daras <getpingui@gmail.com>
2017-03-17 05:20:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get "/newteam" do
+    erb :newteam
+  end
 
+  post "/team" do
+    @center = params[:c]
+    @coach = params[:coach]
+    @point_guard = params[:pg]
+    @power_forward = params[:pf]
+    @shooting_guard = params[:sg]
+    @small_forward = params[:sf]
+    @team_name = params[:name]
 
+    erb :team
+  end
 end
